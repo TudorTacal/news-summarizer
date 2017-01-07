04ee8ca8b368e1799b1d7612cecbd272659e0f2a
TudorTacal <tudortacal@gmail.com>
2017-01-07 15:33:00 +0000

    NewModel saves stories

--- spec/news-model-spec.js
+++ spec/news-model-spec.js
@@ -2,12 +2,17 @@ describe("NewsModel",function (){
 
   var news = new NewsModel ();
 
+  beforeEach(function (){
+    stories = jasmine.createSpy("stories");
+  });
+
   it ("Initializes new objects", function () {
     expect(news instanceof NewsModel).toEqual(true);
   });
 
   it ("Saves stories", function () {
-    
+    news.save(stories);
+    expect(news.storyList.length).toEqual(1);
   });
 
 });
--- src/news-model.js
+++ src/news-model.js
@@ -3,7 +3,13 @@
 (function(exports) {
 
   var NewsModel = function () {
+    this.storyList = [];
+  };
 
+  NewsModel.prototype = {
+    save: function (stories) {
+      this.storyList.push(stories);
+    }
   };
 
   exports.NewsModel = NewsModel;
