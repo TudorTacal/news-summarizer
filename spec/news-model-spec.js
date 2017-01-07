b8277b9a623ebf8e987852a7292aef8de2d4d37c
TudorTacal <tudortacal@gmail.com>
2017-01-07 15:20:40 +0000

    Started to implement the NewsModel

---
+++ spec/news-model-spec.js
@@ -0,0 +1,13 @@
+describe("NewsModel",function (){
+
+  var news = new NewsModel ();
+
+  it ("Initializes new objects", function () {
+    expect(news instanceof NewsModel).toEqual(true);
+  });
+
+  it ("Saves stories", function () {
+    
+  });
+
+});
---
+++ src/news-model.js
@@ -0,0 +1,11 @@
+//saves the stories from the response of the Guardian API call
+
+(function(exports) {
+
+  var NewsModel = function () {
+
+  };
+
+  exports.NewsModel = NewsModel;
+
+})(this);
